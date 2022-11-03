495a1e98b42a3e26b4a3068b1f312c44ba8d2d38
mertaseptiawan <mertaseptiawan@gmail.com>
2022-11-03 14:04:54 +0800

    memperbaiki efek scrolling animate

--- assets/script.js
+++ assets/script.js
@@ -21,3 +21,42 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+        //pop up animate when scrolling
+const observe = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+        //slide right animate when scrolling
+const hidenElements = document.querySelectorAll('.hidden');
+hidenElements.forEach((el) => observe.observe(el));
+
+const observeLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hidenLeft = document.querySelectorAll('.hidden-left');
+hidenLeft.forEach((el) => observe.observe(el));
+
+        //slide left animate when scrolling
+const observeRight = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hidenRight = document.querySelectorAll('.hidden-right');
+hidenRight.forEach((el) => observe.observe(el));
