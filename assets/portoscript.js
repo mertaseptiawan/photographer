3d270134ee98801467adfffad2dd937633b9a858
mertaseptiawan <mertaseptiawan@gmail.com>
2022-11-07 12:09:53 +0800

    memperbaiki animation page portofolio dan membuat file portoscript.js

---
+++ assets/portoscript.js
@@ -0,0 +1,27 @@
+const observe = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiden = document.querySelectorAll('.hidden');
+hiden.forEach((el) => observe.observe(el));
+
+const observeLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hidenLeft = document.querySelectorAll('.hidden-left');
+hidenLeft.forEach((el) => observe.observe(el));
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+    //swipper arrow
 const productContainers = [...document.querySelectorAll('.team-container')];
 const prevBtn = [...document.querySelectorAll('.pre-btn')];
 const nextBtn = [...document.querySelectorAll('.next-btn')];
@@ -15,6 +16,7 @@ productContainers.forEach((item, i) => {
     })
 })
 
+    //menu togle navbar
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
@@ -22,25 +24,29 @@ menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
 
-        //pop up animate when scrolling
+    //pop up animate when scrolling
 const observe = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 });
 
-        //slide right animate when scrolling
 const hidenElements = document.querySelectorAll('.hidden');
 hidenElements.forEach((el) => observe.observe(el));
 
+    //slide right animate when scrolling
 const observeLeft = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 });
@@ -48,15 +54,29 @@ const observeLeft = new IntersectionObserver((entries) => {
 const hidenLeft = document.querySelectorAll('.hidden-left');
 hidenLeft.forEach((el) => observe.observe(el));
 
-        //slide left animate when scrolling
+    //slide left animate when scrolling
 const observeRight = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 });
 
 const hidenRight = document.querySelectorAll('.hidden-right');
 hidenRight.forEach((el) => observe.observe(el));
+
+    //change animate hidden-right to hidden when media query
+// function addClass() {
+//     document.querySelectorAll('.hidden-right');
+//     entry.target.classList.add('hidden')
+//     entry.target.classList.remove('hidden-right')
+// }
+
+// const mediaHidden = window.matchMedia("(max-width: 700px)")
+// if (mediaHidden.matches) {
+//     document.getElementById('hidden').add('hidden');
+// }
